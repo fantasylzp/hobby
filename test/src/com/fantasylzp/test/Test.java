2c6b866b9f14456001c957935cc4fcd4b0defaa6
fantasylzp <fantasylzp@gmail.com>
2014-04-06 11:14:43 +0800

    test

---
+++ test/src/com/fantasylzp/test/Test.java
@@ -0,0 +1,16 @@
+package com.fantasylzp.test;
+
+public class Test
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		System.out.println("test");
+
+	}
+
+}
