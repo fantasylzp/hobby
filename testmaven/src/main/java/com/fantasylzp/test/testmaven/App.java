621e177bf68c943f42873414eb4e93f8ffae1247
fantasylzp <fantasylzp@gmail.com>
2014-04-06 20:10:06 +0800

    commit

--- testmaven/src/main/java/com/fantasylzp/test/testmaven/App.java
+++ testmaven/src/main/java/com/fantasylzp/test/testmaven/App.java
@@ -1,13 +1,18 @@
 package com.fantasylzp.test.testmaven;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    private static final Logger logger = LoggerFactory.getLogger(App.class);
+	public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        logger.debug("hello {}","world");
+    	System.out.println( "Hello World!" );
     }
 }
